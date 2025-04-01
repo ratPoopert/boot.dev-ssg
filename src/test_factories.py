442fa8a0d99127b3e9ce184572290725e8ebfb0c
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2025-03-31 20:57:15 -0400

    Refactor tests for factories module

--- src/test_factories.py
+++ src/test_factories.py
@@ -8,137 +8,124 @@ from factories import (
 )
 
 
-class TestTextNodeToHTMLNode(unittest.TestCase):
-    def test_raises_exception_on_unsupported_text_type(self):
-        node = TextNode("Sample text", None)
-        with self.assertRaises(TypeError):
-            text_node_to_html_node(node)
+class TestFactories(unittest.TestCase):
 
-    def test_returns_leaf_node(self):
+    def test__text_node_to_html_node__no_text_type(self):
+        with self.assertRaises(TypeError):
+            text_node_to_html_node(
+                TextNode("Sample text", None)
+            )
+
+    def test__text_node_to_html_node__normal_text_type(self):
         text_node = TextNode("Sample text", TextType.NORMAL)
         html_node = text_node_to_html_node(text_node)
         self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, text_node.text)
 
-    def test_normal_text_type(self):
-        textnode = TextNode("Sample text", TextType.NORMAL)
-        htmlnode = text_node_to_html_node(textnode)
-        self.assertIsNone(htmlnode.tag)
-        self.assertEqual(htmlnode.value, textnode.text)
+    def test__text_node_to_html_node__bold_text_type(self):
+        text_node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, text_node.text)
 
-    def test_bold_text_type(self):
-        textnode = TextNode("Bold text", TextType.BOLD)
-        htmlnode = text_node_to_html_node(textnode)
-        self.assertEqual(htmlnode.tag, "b")
-        self.assertEqual(htmlnode.value, textnode.text)
+    def test__text_node_to_html_node__italic_text_type(self):
+        text_node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(text_node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, text_node.text)
 
-    def test_italic_text_type(self):
-        textnode = TextNode("Italic text", TextType.ITALIC)
-        htmlnode = text_node_to_html_node(textnode)
-        self.assertEqual(htmlnode.tag, "i")
-        self.assertEqual(htmlnode.value, textnode.text)
+    def test__text_node_to_html_node__code_text_type(self):
+        text_node = TextNode("Code text", TextType.CODE)
+        html_node = text_node_to_html_node(text_node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, text_node.text)
 
-    def test_code_text_type(self):
-        textnode = TextNode("Code text", TextType.CODE)
-        htmlnode = text_node_to_html_node(textnode)
-        self.assertEqual(htmlnode.tag, "code")
-        self.assertEqual(htmlnode.value, textnode.text)
+    def test__text_node_to_html_node__link_text_type(self):
+        text_node = TextNode("Link text",
+                             TextType.LINK,
+                             "https://www.google.com")
+        html_node = text_node_to_html_node(text_node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, text_node.text)
+        self.assertEqual(html_node.props["href"],
+                         text_node.url)
 
-    def test_link_text_type(self):
-        textnode = TextNode("Link text",
-                            TextType.LINK,
-                            "https://www.google.com")
-        htmlnode = text_node_to_html_node(textnode)
-        self.assertEqual(htmlnode.tag, "a")
-        self.assertEqual(htmlnode.value, textnode.text)
-        self.assertEqual(htmlnode.props["href"], textnode.url)
+    def test__text_node_to_html_node__image_text_type(self):
+        text_node = TextNode("Image text",
+                             TextType.IMAGE,
+                             "image.png")
+        html_node = text_node_to_html_node(text_node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props["src"],
+                         text_node.url)
+        self.assertEqual(html_node.props["alt"],
+                         text_node.text)
 
-    def test_image_text_type(self):
-        textnode = TextNode("Image text",
-                            TextType.IMAGE,
-                            "https://google.com/logo.png")
-        htmlnode = text_node_to_html_node(textnode)
-        self.assertEqual(htmlnode.tag, "img")
-        self.assertEqual(htmlnode.props["src"], textnode.url)
-        self.assertEqual(htmlnode.props["alt"], textnode.text)
-
-
-class TestSplitTextNodesByDelimiter(unittest.TestCase):
-
-    def test_skips_non_text_types(self):
-        textnodes = [
-            TextNode("First node", TextType.BOLD),
-            TextNode("Second node", TextType.ITALIC),
-            TextNode("Third node", TextType.CODE),
-            TextNode("Fourth node", TextType.IMAGE, "image.png"),
-            TextNode("Fifth node", TextType.LINK, "www.google.com"),
-        ]
-        result = split_text_nodes_by_delimiter(
-            textnodes,
-            "**",
-            TextType.BOLD,
-        )
-        self.assertListEqual(
-            result,
-            textnodes
-        )
-
-    def test_raises_exception_on_unmatched_delimiter(self):
-        textnodes = [
-            TextNode("Missing a **matching delimiter", TextType.NORMAL),
-        ]
+    def test__split_text_nodes_by_delimiter__unmatched_delimiter(self):
         with self.assertRaises(ValueError):
-            split_text_nodes_by_delimiter(textnodes, "**", TextType.BOLD)
-
-    def test_returns_correct_number_of_textnodes(self):
-        cases = (
-            (
-                TextNode("This is sample text", TextType.NORMAL),
-                1
-            ),
-            (
-                TextNode("This is **sample** text", TextType.NORMAL),
-                3
-            ),
-            (
-                TextNode("**This** is **sample** text", TextType.NORMAL),
-                4
-            ),
-        )
-        for node, expected_count in cases:
-            nodes = split_text_nodes_by_delimiter(
-                [node],
+            split_text_nodes_by_delimiter(
+                [
+                    TextNode("Missing **delimiter", TextType.BOLD),
+                ],
                 "**",
                 TextType.BOLD,
             )
-            self.assertEqual(len(nodes), expected_count)
 
-    def test_handles_delimiters_mid_text(self):
-        nodes = [TextNode("This has **bold** text.", TextType.NORMAL)]
-        expected = [
-            TextNode("This has ", TextType.NORMAL),
-            TextNode("bold", TextType.BOLD),
-            TextNode(" text.", TextType.NORMAL),
-        ]
-        self.assertListEqual(
-            split_text_nodes_by_delimiter(nodes, "**", TextType.BOLD),
-            expected,
-        )
+    def test__split_text_nodes_by_delimiter__formatted_text_types(self):
 
-    def test_handles_delimiter_at_start_of_text(self):
         nodes = [
-            TextNode(
-                "**This** delimiter is at the start.",
-                TextType.NORMAL
-            )
+            TextNode("Bold node", TextType.BOLD),
+            TextNode("Italic node", TextType.ITALIC),
+            TextNode("Code node", TextType.CODE),
+            TextNode("Image node", TextType.IMAGE, "image.png"),
+            TextNode("Link node", TextType.LINK, "www.google.com"),
         ]
-        expected = [
-            TextNode("This", TextType.BOLD),
-            TextNode(" delimiter is at the start.", TextType.NORMAL),
-        ]
-        self.assertListEqual(
-            split_text_nodes_by_delimiter(nodes, "**", TextType.BOLD),
-            expected,
+        self.assertEqual(
+            nodes,
+            split_text_nodes_by_delimiter(nodes, "**", TextType.BOLD)
         )
+
+    def test__split_text_nodes_by_delimiter__normal_text(self):
+
+        for text_type, d in (
+            (TextType.BOLD, "**"),
+            (TextType.ITALIC, "_"),
+            (TextType.CODE, "`"),
+        ):
+            strings = [
+                "This is normal text.",
+                f"This has {d}special{d} text.",
+                f"{d}This{d} delimiter is at the start.",
+                f"This delimiter is at the {d}end.{d}",
+                f"{d}This{d} has {d}two{d} delimiters.",
+            ]
+            nodes = list(map(lambda s: TextNode(s, TextType.NORMAL), strings))
+            expected = [
+                TextNode("This is normal text.", TextType.NORMAL),
+                TextNode("This has ", TextType.NORMAL),
+                TextNode("special", text_type),
+                TextNode(" text.", TextType.NORMAL),
+                TextNode("This", text_type),
+                TextNode(" delimiter is at the start.", TextType.NORMAL),
+                TextNode("This delimiter is at the ", TextType.NORMAL),
+                TextNode("end.", text_type),
+                TextNode("This", text_type),
+                TextNode(" has ", TextType.NORMAL),
+                TextNode("two", text_type),
+                TextNode(" delimiters.", TextType.NORMAL),
+            ]
+
+            self.assertListEqual(
+                split_text_nodes_by_delimiter(nodes, d, text_type),
+                expected
+            )
 
 
 if __name__ == "__main__":
